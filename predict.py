7865b289bc6fd62bfe3eb9d0b72e5d6293a9a581
Chonwai <chonwaiun@gmail.com>
2025-04-19 21:22:59 +0800

    Update: Updated the formatter

--- predict.py
+++ predict.py
@@ -15,9 +15,10 @@ from scipy.spatial.distance import pdist
 import matplotlib.pyplot as plt
 import os
 import collections
-from model_def import TOXI_REG, DNN_module #, freeze
+from model_def import TOXI_REG, DNN_module  # , freeze
 from toxi_dataloader import *
-from toxi_dataloader import _get_train_data_loader, _get_test_data_loader #, freeze
+from toxi_dataloader import _get_train_data_loader, _get_test_data_loader  # , freeze
+
 
 def CCC(y_true, y_pred):
     cor = np.corrcoef(y_true, y_pred)[0][1]
@@ -32,46 +33,92 @@ def CCC(y_true, y_pred):
     denominator = var_true + var_pred + (mean_true - mean_pred) ** 2
     return numerator / denominator
 
-def lysis_metric_cal(y_true, y_pred, onehot_species_list, species_type, onehot_lysis_list, lysis_type):
-    if species_type == 'Human':
-        if lysis_type == 'HC50':
-            idx = [i for i in range(len(onehot_lysis_list)) if (str(onehot_species_list[i]) == '[1. 0. 0. 0. 0. 0.]') & (str(onehot_lysis_list[i]) == '[1. 0. 0.]')]
-        elif lysis_type == 'HC10':
-            idx = [i for i in range(len(onehot_lysis_list)) if (str(onehot_species_list[i]) == '[1. 0. 0. 0. 0. 0.]') & (str(onehot_lysis_list[i]) == '[0. 1. 0.]')]
-        elif lysis_type == 'HC5':
-            idx = [i for i in range(len(onehot_lysis_list)) if (str(onehot_species_list[i]) == '[1. 0. 0. 0. 0. 0.]') & (str(onehot_lysis_list[i]) == '[0. 0. 1.]')]
+
+def lysis_metric_cal(
+    y_true, y_pred, onehot_species_list, species_type, onehot_lysis_list, lysis_type
+):
+    if species_type == "Human":
+        if lysis_type == "HC50":
+            idx = [
+                i
+                for i in range(len(onehot_lysis_list))
+                if (str(onehot_species_list[i]) == "[1. 0. 0. 0. 0. 0.]")
+                & (str(onehot_lysis_list[i]) == "[1. 0. 0.]")
+            ]
+        elif lysis_type == "HC10":
+            idx = [
+                i
+                for i in range(len(onehot_lysis_list))
+                if (str(onehot_species_list[i]) == "[1. 0. 0. 0. 0. 0.]")
+                & (str(onehot_lysis_list[i]) == "[0. 1. 0.]")
+            ]
+        elif lysis_type == "HC5":
+            idx = [
+                i
+                for i in range(len(onehot_lysis_list))
+                if (str(onehot_species_list[i]) == "[1. 0. 0. 0. 0. 0.]")
+                & (str(onehot_lysis_list[i]) == "[0. 0. 1.]")
+            ]
     y_true_lysis = [y_true[i] for i in idx]
     y_pred_lysis = [y_pred[i] for i in idx]
     mse = mean_squared_error(y_true_lysis, y_pred_lysis)
-    pcc= pearsonr(y_true_lysis, y_pred_lysis)[0]
+    pcc = pearsonr(y_true_lysis, y_pred_lysis)[0]
     ktc = kendalltau(y_true_lysis, y_pred_lysis)[0]
     ccc = CCC(y_true_lysis, y_pred_lysis)
     r2 = r2_score(y_true_lysis, y_pred_lysis)
     return mse, pcc, ktc, ccc, r2
 
-def csv_lysis_metric_cal(y_true, y_pred, onehot_species_list, species_type, onehot_lysis_list, lysis_type):
-    if species_type == 'Human':
-        if lysis_type == 'HC50':
-            idx = [i for i in range(len(onehot_lysis_list)) if (str(onehot_species_list[i]) == '[1 0 0 0 0 0]') & (str(onehot_lysis_list[i]) == '[1 0 0]')]
-        elif lysis_type == 'HC10':
-            idx = [i for i in range(len(onehot_lysis_list)) if (str(onehot_species_list[i]) == '[1 0 0 0 0 0]') & (str(onehot_lysis_list[i]) == '[0 1 0]')]
-        elif lysis_type == 'HC5':
-            idx = [i for i in range(len(onehot_lysis_list)) if (str(onehot_species_list[i]) == '[1 0 0 0 0 0]') & (str(onehot_lysis_list[i]) == '[0 0 1]')]
+
+def csv_lysis_metric_cal(
+    y_true, y_pred, onehot_species_list, species_type, onehot_lysis_list, lysis_type
+):
+    if species_type == "Human":
+        if lysis_type == "HC50":
+            idx = [
+                i
+                for i in range(len(onehot_lysis_list))
+                if (str(onehot_species_list[i]) == "[1 0 0 0 0 0]")
+                & (str(onehot_lysis_list[i]) == "[1 0 0]")
+            ]
+        elif lysis_type == "HC10":
+            idx = [
+                i
+                for i in range(len(onehot_lysis_list))
+                if (str(onehot_species_list[i]) == "[1 0 0 0 0 0]")
+                & (str(onehot_lysis_list[i]) == "[0 1 0]")
+            ]
+        elif lysis_type == "HC5":
+            idx = [
+                i
+                for i in range(len(onehot_lysis_list))
+                if (str(onehot_species_list[i]) == "[1 0 0 0 0 0]")
+                & (str(onehot_lysis_list[i]) == "[0 0 1]")
+            ]
     y_true_lysis = [y_true[i] for i in idx]
     y_pred_lysis = [y_pred[i] for i in idx]
     mse = mean_squared_error(y_true_lysis, y_pred_lysis)
-    pcc= pearsonr(y_true_lysis, y_pred_lysis)[0]
+    pcc = pearsonr(y_true_lysis, y_pred_lysis)[0]
     ktc = kendalltau(y_true_lysis, y_pred_lysis)[0]
     ccc = CCC(y_true_lysis, y_pred_lysis)
     r2 = r2_score(y_true_lysis, y_pred_lysis)
     return mse, pcc, ktc, ccc, r2
 
-def csv_best_lysis_metric_record(best_target_list, best_predict_list, best_species_list, best_lysis_list):
+
+def csv_best_lysis_metric_record(
+    best_target_list, best_predict_list, best_species_list, best_lysis_list
+):
     lysis_list, mse_list, pcc_list, ktc_list, ccc_list, r2_list = [], [], [], [], [], []
-    hc_list = ['HC5', 'HC10', 'HC50']
+    hc_list = ["HC5", "HC10", "HC50"]
     for hc in hc_list:
         lysis_list.append(hc)
-        mse, pcc, ktc, ccc, r2 = csv_lysis_metric_cal(best_target_list, best_predict_list, best_species_list, 'Human', best_lysis_list, hc)
+        mse, pcc, ktc, ccc, r2 = csv_lysis_metric_cal(
+            best_target_list,
+            best_predict_list,
+            best_species_list,
+            "Human",
+            best_lysis_list,
+            hc,
+        )
         mse_list.append(mse)
         pcc_list.append(pcc)
         ktc_list.append(ktc)
@@ -79,18 +126,29 @@ def csv_best_lysis_metric_record(best_target_list, best_predict_list, best_speci
         r2_list.append(r2)
     return lysis_list, mse_list, pcc_list, ktc_list, ccc_list, r2_list
 
-def best_lysis_metric_record(best_target_list, best_predict_list, best_species_list, best_lysis_list):
+
+def best_lysis_metric_record(
+    best_target_list, best_predict_list, best_species_list, best_lysis_list
+):
     lysis_list, mse_list, pcc_list, ktc_list, ccc_list, r2_list = [], [], [], [], [], []
-    hc_list = ['HC5', 'HC10', 'HC50']
+    hc_list = ["HC5", "HC10", "HC50"]
     for hc in hc_list:
         lysis_list.append(hc)
-        mse, pcc, ktc, ccc, r2 = lysis_metric_cal(best_target_list, best_predict_list, best_species_list, 'Human', best_lysis_list, hc)
+        mse, pcc, ktc, ccc, r2 = lysis_metric_cal(
+            best_target_list,
+            best_predict_list,
+            best_species_list,
+            "Human",
+            best_lysis_list,
+            hc,
+        )
         mse_list.append(mse)
         pcc_list.append(pcc)
         ktc_list.append(ktc)
         ccc_list.append(ccc)
         r2_list.append(r2)
     return lysis_list, mse_list, pcc_list, ktc_list, ccc_list, r2_list
+
 
 def fasta2csv(fasta_path, csv_path):
     f = open(fasta_path, "r")
@@ -99,9 +157,9 @@ def fasta2csv(fasta_path, csv_path):
         if line.startswith(">"):
             name = line.split()[0]
             name = name.replace(">", "")
-            seq[name] = ''
+            seq[name] = ""
         else:
-            seq[name] += line.replace("\n", '').strip()
+            seq[name] += line.replace("\n", "").strip()
     f.close()
     ids_list = []
     sequences_list = []
@@ -117,15 +175,18 @@ def fasta2csv(fasta_path, csv_path):
             sequences_space_list.append(" ".join(sequence))
             lysis_list.append(lysis)
 
-    seq_df = pd.DataFrame({
-        'ID': ids_list,
-        'SEQUENCE': sequences_list,
-        'SEQUENCE_space': sequences_space_list,
-        'lysis': lysis_list
-    })
-    seq_df['species'] = ['Human'] * len(seq_df)
+    seq_df = pd.DataFrame(
+        {
+            "ID": ids_list,
+            "SEQUENCE": sequences_list,
+            "SEQUENCE_space": sequences_space_list,
+            "lysis": lysis_list,
+        }
+    )
+    seq_df["species"] = ["Human"] * len(seq_df)
     seq_df.to_csv(csv_path, index=False)
     return seq_df
+
 
 def csv2fasta_func(csv_path, fasta_path):
     # get .csv info
@@ -137,38 +198,46 @@ def csv2fasta_func(csv_path, fasta_path):
         fast_file.write(seq_data.SEQUENCE[i] + "\n")
     fast_file.close()
 
+
 def predict(pretrain_model_path, model_path, input_csv_path, output_csv_path):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     # device = torch.device('mps')
     pretrain_bert = torch.load(pretrain_model_path)
     dnn = DNN_module(species_len=6, lysis_len=3, dropout=0.5)
     model = TOXI_REG(pretrain_bert=pretrain_bert, dnn=dnn)
-    model.load_state_dict(torch.load(model_path, map_location = device))
+    model.load_state_dict(torch.load(model_path, map_location=device))
     model = model.to(device)
 
     test_df = pd.read_csv(input_csv_path)
-    if 'SEQUENCE_space' not in test_df.columns:
-        test_df['SEQUENCE_space'] = [" ".join(ele) for ele in test_df['SEQUENCE']]
+    if "SEQUENCE_space" not in test_df.columns:
+        test_df["SEQUENCE_space"] = [" ".join(ele) for ele in test_df["SEQUENCE"]]
         test_df.to_csv(input_csv_path, index=False)
-    if 'pHC' not in test_df.columns:
-        test_df['pHC'] = 0 * len(test_df)
+    if "pHC" not in test_df.columns:
+        test_df["pHC"] = 0 * len(test_df)
         test_df.to_csv(input_csv_path, index=False)
     test_loader = _get_test_data_loader(500, input_csv_path)
-    test_predict_list, test_target_list, onehot_species_list, onehot_lysis_list = [], [], [], []
+    test_predict_list, test_target_list, onehot_species_list, onehot_lysis_list = (
+        [],
+        [],
+        [],
+        [],
+    )
     # seq_feature = [] #np.empty((0, 1024), dtype="float32")
 
     model.eval()
     with torch.no_grad():
         for batch in test_loader:
-            b_input_ids = batch['input_ids'].to(device)
-            b_input_mask = batch['attention_mask'].to(device)
-            b_labels = batch['targets']
-            b_species = batch['onehot_species'].to(device)
-            b_lysis = batch['onehot_lysis'].to(device)
-            predict_pHC, cls_embedding = model(input_ids=b_input_ids,
-                                               attention_mask=b_input_mask,
-                                               onehot_species=b_species,
-                                               onehot_lysis=b_lysis)
+            b_input_ids = batch["input_ids"].to(device)
+            b_input_mask = batch["attention_mask"].to(device)
+            b_labels = batch["targets"]
+            b_species = batch["onehot_species"].to(device)
+            b_lysis = batch["onehot_lysis"].to(device)
+            predict_pHC, cls_embedding = model(
+                input_ids=b_input_ids,
+                attention_mask=b_input_mask,
+                onehot_species=b_species,
+                onehot_lysis=b_lysis,
+            )
 
             onehot_species_list.append(b_species.cpu().data.numpy())
             onehot_lysis_list.append(b_lysis.cpu().data.numpy())
@@ -184,24 +253,26 @@ def predict(pretrain_model_path, model_path, input_csv_path, output_csv_path):
     test_predict_list = np.concatenate(test_predict_list).flatten()
     # test_target_list = np.concatenate(test_target_list).flatten()
 
-    test_df['predicted_pHC'] = test_predict_list
-    test_df["predicted_HC"] = [10 ** (-item) for item in test_predict_list]  # transform pHC value to HC
-    test_df = test_df[['ID', 'SEQUENCE', 'species', 'lysis', 'predicted_HC']]
+    test_df["predicted_pHC"] = test_predict_list
+    test_df["predicted_HC"] = [
+        10 ** (-item) for item in test_predict_list
+    ]  # transform pHC value to HC
+    test_df = test_df[["ID", "SEQUENCE", "species", "lysis", "predicted_HC"]]
     test_df.to_csv(output_csv_path, index=False)
     return test_df
 
 
 if __name__ == "__main__":
     # load the final DAPT+prefix+task_finetune model
-    pretrain_model_path =  'toxic_pep_prot_bert.pth'
-    model_path = 'proposed_model_reproduce.pkl'
+    pretrain_model_path = "toxic_pep_prot_bert.pth"
+    model_path = "proposed_model_reproduce.pkl"
 
     # load an input fasta file and store sequences in an input csv file
-    input_fasta_path = 'example_input.fasta'
-    input_csv_path = 'example_input.csv'
+    input_fasta_path = "example_input.fasta"
+    input_csv_path = "example_input.csv"
     fasta2csv(input_fasta_path, input_csv_path)
 
     # predict sequences, and store prediction result in an output csv file
-    output_csv_path = 'example_output.csv'
+    output_csv_path = "example_output.csv"
     test_df = predict(pretrain_model_path, model_path, input_csv_path, output_csv_path)
     print("smart")
